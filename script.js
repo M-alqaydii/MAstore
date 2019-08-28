1b3bab46ffce3f1e52cb24fc126161b9393c8387
AMalnaqbi <amalnaqbi.a3@gmail.com>
2019-08-28 08:41:28 +0400

    First commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
+
+/// fetch the product array using JSON.
+
+// fetch("./product.json")
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+fetch("./product.json")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    createTable(data.products);
+  });
+
+//Create Table
+function createTable(records) {
+  var table = document.createElement("table");
+  table.setAttribute("border", 1);
+  table.appendChild(createHeading(records[0]));
+  for (let record of records) {
+    table.appendChild(createRow(record));
+  }
+  document.getElementById("container").appendChild(table);
+}
+
+function createHeading(record) {
+  var row = document.createElement("tr");
+  for (let prop in record) {
+    var heading = document.createElement("th");
+    heading.innerHTML = prop.toUpperCase();
+    row.appendChild(heading);
+  }
+  return row;
+}
+
+function createRow(record) {
+  var row = document.createElement("tr");
+  for (let prop in record) {
+    var column = document.createElement("td");
+    column.innerHTML = record[prop];
+    row.appendChild(column);
+  }
+  return row;
+}
